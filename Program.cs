066b9f1364c40d6b4ae1ae94c86289dee2d10bf1
pasalvadhisreepriya <sreepriyapasalvadhi@gmail.com>
2023-04-22 19:52:18 +0530

    Project

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿ using System;  
+public class def 
+{  
+    public static void Main(string [] args)
+    {  
+   
+        List<string> S = new List<string>()  
+        {  
+           "roses are red", "bottle also red", "violets blue", "Laptop"
+        };
+
+        // Query syntax
+
+        var r = from P in S  
+                where P.Contains("red")  
+                select P;    
+        foreach(var i in r)  
+        {  
+            Console.WriteLine(i);  
+        }  
+
+         // Method syntax
+
+         var r1 = S.Where(a=> a.Contains("red")); 
+        foreach(var i in r1)  
+        {  
+            Console.WriteLine(i);  
+        }  
+    }  
+}  
+
